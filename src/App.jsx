1ac50d3a6179fe82c74b4906c20030d789b837e5
AdamDCosta <adam.dcosta@googlemail.com>
2021-09-30 16:37:09 +0100

    component plan written, nav component files added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 
 import './App.scss';
+import Nav from './components/Nav/Nav';
 
 const App = () => {
   return (
     <div className="App">
-      
+      <Nav />
     </div>
   );
 }
 
 export default App;
+
+
+// The Plan
+
+// Clean up files - add app.jsx and app.scss
+
+// Add data file
+
+// Plan components & props
+
+// --> Components/props
+//  1. Sidenav
+//      -> Search Bar
+//      -> Filter List
+//        -> Filter Item
+//  2. Main section (container component)
+//      -> Card List
+//      -> Beer Card
+
