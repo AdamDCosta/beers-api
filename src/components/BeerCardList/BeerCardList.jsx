fdf0ede72960ba3fe2734badc4e69bb445368d5c
AdamDCosta <adam.dcosta@googlemail.com>
2021-10-02 15:34:54 +0100

    beer tile flex box

--- src/components/BeerCardList/BeerCardList.jsx
+++ src/components/BeerCardList/BeerCardList.jsx
@@ -8,12 +8,14 @@ const BeerCardList = (props) => {
   const { beersArr } = props;
 
   const cardListJSX = beersArr.map((beer, index) => {
-    return <BeerTile key={beer.name + (index + 1)} beer = { beer }/>
+    return <BeerTile key={beer.name + (index + 1)} beer = { beer }/> 
   })
   return (
 
     <section className="beers">
-      {cardListJSX}
+      
+        {cardListJSX}
+      
     </section>
   )
 }
--- src/components/BeerTile/BeerTile.jsx
+++ src/components/BeerTile/BeerTile.jsx
@@ -9,20 +9,24 @@ const BeerTile = (props) => {
   
   return (
     <article className="beer-tile">
-      {/* do I need to add key here or when I iterate? */}
-      <img className="beer-tile__image" src={beer.image_url} alt={beer.name} />
-      <div className="beer-tile__heading">
-        <h2>{beer.name}</h2>
-        <p>{beer.tagline}</p>
+      <div className="beer-tile__left">
+        <img className="beer-tile__left--image" src={beer.image_url} alt={beer.name} />
       </div>
-      <p>{beer.description}</p>
-      <div className=".beer-tile__info">
+      <div className="beer-tile__right">
+        <div className="beer-tile__right--heading">
+          <h2>{beer.name}</h2>
+          <h4>{beer.tagline}</h4>
+        </div>
+        <p>{beer.description}</p>
+        <div className=".beer-tile__right--info">
         <h4>ABV:
-          <span>{beer.abv}%</span>
+          <span> {beer.abv}%</span>
         </h4>
-        <h4>First Brewed 
-          <span>{beer.first_brewed}</span> 
+        <h4>First Brewed: 
+          <span> {beer.first_brewed}</span> 
         </h4>
+      </div>
+
       </div>
 
     </article>
