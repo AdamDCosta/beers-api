f8f544135ba1b32dc2daf6dc3e9fff7e58a79a7b
AdamDCosta <adam.dcosta@googlemail.com>
2021-10-03 10:48:06 +0100

    beer.name to upper case

--- src/components/BeerTile/BeerTile.jsx
+++ src/components/BeerTile/BeerTile.jsx
@@ -14,7 +14,7 @@ const BeerTile = (props) => {
       </div>
       <div className="beer-tile__right">
         <div className="beer-tile__right--heading">
-          <h2>{beer.name}</h2>
+          <h2>{beer.name.toUpperCase()}</h2>
           <h4>{beer.tagline}</h4>
         </div>
         <p>{beer.description}</p>
