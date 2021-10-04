ba452d48548f49fdb087d23e2aaaeba6d9e9f844
AdamDCosta <adam.dcosta@googlemail.com>
2021-10-04 17:02:03 +0100

    classic range component added

---
+++ src/components/ClassicRange/ClassicRange.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./ClassicRange.scss"
+
+const ClassicRange = () => {
+  return (
+    
+    <div className="classic">
+      <label htmlFor="classic">Classic Range</label>
+      <input type="checkbox" name="classic" />
+    </div>
+    
+  )
+}
+
+export default ClassicRange
--- src/components/FiltersList/FiltersList.jsx
+++ src/components/FiltersList/FiltersList.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import HighABV from '../HighABV/HighABV'
+import ClassicRange from '../ClassicRange/ClassicRange'
 
 const FiltersList = () => {
 
@@ -8,6 +9,7 @@ const FiltersList = () => {
   return (
     <div className="filters-list">
       <HighABV />
+      <ClassicRange />
     </div>
   )
 }
