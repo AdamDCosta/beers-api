b07260af50d566821822cd2bce37754590ee08aa
AdamDCosta <adam.dcosta@googlemail.com>
2021-10-06 12:34:01 +0100

    console.log fetch request working

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,17 @@ import Main from "./components/Main/Main";
 
 
 const App = () => {
+
+  // const [ beers, setBeers ] = useState([])
+
+  fetch("https://api.punkapi.com/v2/beers?page=1&per_page=80")
+  .then(response => {
+    return response.json()
+  })
+  .then(beersObj => {
+    console.log(beersObj)
+  })
+
   return (
     <div className="App">
       <Header />
