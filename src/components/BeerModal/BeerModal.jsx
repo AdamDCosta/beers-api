cbec20a587794c03e800f821175c88d93539ba05
AdamDCosta <adam.dcosta@googlemail.com>
2021-10-10 15:05:21 +0100

    beer modal styling finished - footer added

--- src/components/BeerModal/BeerModal.jsx
+++ src/components/BeerModal/BeerModal.jsx
@@ -3,7 +3,8 @@ import "./BeerModal.scss"
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import whiteCross from "../../assets/images/white-cross.png"
-
+import Header from '../Header/Header';
+import whiteBanner from "../../assets/images/brewdog_white.png"
 
 const BeerModal = (props) => {
 
@@ -18,9 +19,22 @@ const BeerModal = (props) => {
 
   console.log(idObj)
 
+  const maltsList = idObj.ingredients.malt.map(mlt => {
+    const maltItem =  <li>{mlt.name}: {mlt.amount.value}kg</li>
+    return maltItem
+  })
+
+  const hopsList = idObj.ingredients.hops.map(hop => {
+    const hopsItem =  <li>{hop.name}: {hop.amount.value}kg</li>
+    return hopsItem
+  })
+
 
   return (
-    <article className="beer-modal">
+
+    <section className="beer-modal">
+
+      <Header />
 
       <Link to="/">
         <img className="beer-modal__close" src={whiteCross} alt="close window"/>
@@ -36,36 +50,69 @@ const BeerModal = (props) => {
           <h4 className="beer-modal__heading--tag">
             {`"${idObj.tagline}"`}
           </h4>
-        </div>
-
-      
-      
-        <p>{idObj.description}</p>
-        <div className="beer-modal__info">
-          <h4>ABV:
-            <span> {idObj.abv}%</span>
-          </h4>
-          <h4>IBU:
-            <span> {idObj.ibu}</span>
-          </h4>
-          <h4>EBC:
-            <span> {idObj.ebc}</span>
-          </h4>
-        </div>
-
-        <h2>Great to drink with...</h2>
-          <ul className="beer-modal__pairings">
-            <li>{idObj.food_pairing[0]}</li>
-            <li>{idObj.food_pairing[1]}</li>
-            <li>{idObj.food_pairing[2]}</li>
-          </ul>
-        <h4>First Brewed: 
+          <h4 className="beer-modal__heading--date">First Brewed: 
           <span> {idObj.first_brewed}</span> 
         </h4>
+        </div>
+
+        <div className="beer-modal__desc">
+          <p>{idObj.description}</p>
+        </div>
+
+        <div className="beer-modal__grid">
+          <div className="beer-modal__info">        
+            <h4 className="beer-modal__info--cell">
+              ABV:<span> {idObj.abv}%</span>
+            </h4>         
+            <h4 className="beer-modal__info--cell">
+              IBU:<span> {idObj.ibu}</span>
+            </h4>          
+            <h4 className="beer-modal__info--cell">
+              EBC:<span> {idObj.ebc}</span>
+            </h4>         
+          </div>
+        </div>
+
+        <div className="beer-modal__pairings">
+          <h2>Drink with...</h2>
+            <ul>
+              <li>{idObj.food_pairing[0]}</li>
+              <li>{idObj.food_pairing[1]}</li>
+              <li>{idObj.food_pairing[2]}</li>
+            </ul>
+        </div>
+        
+
+        <article className="beer-modal__brewing">
+          <h2 className="beer-modal__brewing--heading">Brewers Tips</h2>
+          <p className="beer-modal__brewing--tip">
+            {idObj.brewers_tips}
+          </p>
+          <div className="beer-modal__brewing--ingredients">
+            <div className="beer-modal__brewing--ingredients-cell">
+              <h4>Malts</h4>
+              <ul>
+               {maltsList}
+              </ul>
+            </div>
+            <div className="beer-modal__brewing--ingredients-cell">
+              <h4>Hops</h4>
+              <ul>
+               {hopsList}
+              </ul>
+            </div>
+
+
+          </div>
+        </article>
       
 
-
-    </article>
+    <Link to="/">
+      <footer className="beer-modal__footer">
+        <img src={whiteBanner} alt="brewdog footer" />
+      </footer>
+    </Link>
+    </section>
   )
 }
 
